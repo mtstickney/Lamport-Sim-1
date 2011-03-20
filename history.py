6cb8e74cb627c200d5b12cb8a219474acbaa78c7
Matthew Stickney <fiendishlinux@gmail.com>
2011-03-20 12:35:49 -0400

    Use deque instead of cons-list

--- history.py
+++ history.py
@@ -18,27 +18,25 @@ class History:
 
     Only changed items are stored in each state, to reduce storage costs."""
     def __init__(self, initial_state=None):
-        # The history is a cons-list of state dicts (two-tuple == cons cell)
-        # hist[0] == (car hist), hist[1] == (cdr hist)
-        self.hist = ()
+        self.hist = collections.deque()
         # Tail holds future events when we step backwards
         self.tail = collections.deque()
         self.cache = None
         if initial_state is not None:
-            self.hist = (initial_state, self.hist)
+            self.hist.append(initial_state)
             self.cache = initial_state
 
     def __iter__(self):
         "Return iterator for stored events, from newer to older"
-        return itertools.chain(reversed(self.tail), clist_iter(self.hist))
+        return itertools.chain(reversed(self.tail), iter(self.hist))
 
     def __len__(self):
         "Returns the number of states in the history"
-        return clist_len(self.hist)+len(self.tail)
+        return len(self.hist)+len(self.tail)
 
     def past_len(self):
         "Returns the number of past or current states"
-        return clist_len(self.hist)
+        return len(self.hist)
 
     # Get key is passed the new state dict and the key to be inserted
     # Returns the dictionary in which to insert the key
@@ -52,7 +50,7 @@ class History:
             c[k] = v
         if len(self.tail) > 0:
             self.tail.clear()
-        self.hist = (newhist, self.hist)
+        self.hist.append(newhist)
         self.cache = None
 
     def fill_cache(self):
@@ -89,33 +87,16 @@ class History:
         """Move back n positions in the history.
 
         If there aren't n past items in the history, move back until we reach the initial state or empty history."""
-        if not self.hist:
-            return
-        for i in range(n):
-            if not self.hist[1]:
-                return
-            self.tail.append(self.hist[0])
-            self.hist = self.hist[1]
+        for i in range(min(n, len(self.hist)-1)):
+            self.tail.append(self.hist.pop())
         self.cache = None
+        assert len(self.hist) >= 1
 
     def forward(self, n=1):
         "Move forward n positions in the history."
         for i in range(min(n, len(self.tail))):
-            v = self.tail.pop()
-            self.hist = (v, self.hist)
+            self.hist.append(self.tail.pop())
         self.cache = None
-
-def clist_iter(clst):
-    while clst:
-        yield clst[0]
-        clst = clst[1]
-
-def clist_len(clst):
-    n = 0
-    while clst:
-        n += 1
-        clst = clst[1]
-    return n
 
 # Optional getkey parameter is used to filter the history state if non-None
 # Ex: getkey=(lambda s: return s["procs"]) will make search the procs dict
@@ -124,10 +105,8 @@ def hist_find(history, key, getkey=(lambda s: s)):
     """Find the most recent value for a key in the history.
 
     The dictionary returned by getkey() is searched."""
-    val = None
-    for s in clist_iter(history):
+    for s in history:
         d = getkey(s)
         if key in d:
-            val = d[key]
-            break
-    return val
+            return d[key]
+    return None

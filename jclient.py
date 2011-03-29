fa2b3099838b82ba7112d5aac3d93a21367d4a93
Matthew Stickney <fiendishlinux@gmail.com>
2011-03-29 12:30:16 -0400

    Use boundjid propety instead of deprecated jid
    Fixes issue where resource string prevents the bot from filtering messages from itself

--- jclient.py
+++ jclient.py
@@ -15,36 +15,39 @@ class SimBot(sleekxmpp.ClientXMPP):
 
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("groupchat_message", self.groupchat_message)
-        self.add_event_handler("message", self.message)
+        # self.add_event_handler("message", self.message)
         self.add_event_handler("sim_event", self.local_event)
 
     def start(self, event):
         print("Starting session")
         self.send_presence(pstatus='Available')
-        self['xep_0045'].joinMUC(self.mucroom, self.jid)
+        self['xep_0045'].joinMUC(self.mucroom, self.boundjid.bare)
 
     def message(self, msg):
         print("Received message '{}'".format(msg))
         if msg.get_type() is not 'chat':
-            util.warn("Ignoring message of type '"+msg.get_type()+"'")
+            util.warn("message(): Ignoring message of type '"+msg.get_type()+"'")
             return
-        if msg['from'] is not self.jid:
+        if msg['from'] is not self.boundjid.bare:
             self.msg_queue.put(json.loads(msg.body))
 
     def groupchat_message(self, msg):
-        print("Got group message '{}'".format(msg))
-        if msg.get_type() is not 'groupchat':
-            util.warn("Ignoring message of type '"+ msg.get_type() + "'")
+        if msg['to'].bare == self.boundjid.bare:
             return
-        if msg['from'] is not self.jid:
-            self.msg_queue.put(json.loads(msg.body), True)
+        print("Got group message '{}'".format(msg))
+        if msg.get_type() != 'groupchat':
+            util.warn("groupchat_message(): Ignoring message of type '"+ msg.get_type() + "'")
+            print(type(msg.get_type()))
+            return
+        if msg['from'] is not self.boundjid.bare:
+            self.msg_queue.put(json.loads(msg['body']), True)
 
     def local_event(self, data):
         print("Sending message to group: '{}'".format(json.dumps(data)))
         self.send_message(mto=self.mucroom, mbody=json.dumps(data),
-                          mfrom=self.jid, mtype='groupchat')
+                          mfrom=self.boundjid.bare, mtype='groupchat')
         # for c in self.client_lst:
         #     print("Sending message to client {}".format(c))
         #     self.send_message(mto=c, mbody=json.dumps(data),
-        #                       mfrom=self.jid, mtype='chat')
+        #                       mfrom=self.boundjid.bare, mtype='chat')
 
